c927d018ef7cefe77d46703be0d46b50cf8e1e09
PatrykGrudzien <patryk.grudzien.91@gmail.com>
2019-03-14 14:06:29 +0100

    Regenerated project initial files using "create-react-app" command

--- src/App.js
+++ src/App.js
@@ -9,10 +9,16 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-              Hi There!
-              <hr/>
-              docker-compose works with volumes!
+            Edit <code>src/App.js</code> and save to reload.
           </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
         </header>
       </div>
     );
